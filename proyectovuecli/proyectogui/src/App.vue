72cc0787a3af37d17928c86f72be396364d6c4a5
educalo77 <educalo@outlook.com>
2021-02-12 13:06:48 -0300

    axios en vue y como renderizar la data

--- proyectovuecli/proyectogui/src/App.vue
+++ proyectovuecli/proyectogui/src/App.vue
@@ -3,16 +3,46 @@
     <Persona nombre="Edu" edad="43"></Persona>
     <hr />
     <Persona nombre="Ami" edad="10"></Persona>
+    <hr />
+    <hr />
+    <h1>Usuarios:</h1>
+    <Usuarios
+      v-for="usuario in usuarios"
+      v-bind:key="usuario.id"
+      :nombre="usuario.name"
+      :email="usuario.email"
+      :telefono="usuario.phone"
+    >
+    </Usuarios>
+    <hr />
   </div>
 </template>
 
 <script>
 import Persona from "./components/Persona";
+import axios from "axios";
+import Usuarios from "./components/Usuarios";
 
 export default {
   name: "App",
   components: {
     Persona,
+    Usuarios,
+  },
+  data() {
+    return {
+      usuarios: [],
+    };
+  },
+  methods: {
+    obtenerusuarios() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => (this.usuarios = res.data));
+    },
+  },
+  mounted() {
+    this.obtenerusuarios();
   },
 };
 </script>
---
+++ proyectovuecli/proyectogui/src/components/Usuarios.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Nombre: {{ nombre }}</p>
+    <p>Email: {{ email }}</p>
+    <p>Telefono: {{ telefono }}</p>
+    <hr />
+  </div>
+</template>
+<script>
+export default {
+  name: "Usuarios",
+  props: {
+    nombre: {
+      type: String,
+      require: true,
+      default: "Edu",
+    },
+    email: String,
+    telefono: String,
+  },
+};
+</script>
+
+<style scoped></style>
