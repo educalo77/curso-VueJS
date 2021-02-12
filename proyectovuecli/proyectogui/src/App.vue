386da454974582f252075b14436e48cd11ef407b
educalo77 <educalo@outlook.com>
2021-02-12 11:06:48 -0300

    empezando a trabajar con vue-cli

---
+++ proyectovuecli/proyectogui/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Saludo></Saludo>
+  </div>
+</template>
+
+<script>
+import Saludo from "./components/Saludos";
+
+export default {
+  name: "App",
+  components: {
+    Saludo,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ proyectovuecli/proyectogui/src/components/Saludos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Soy el componente Saludo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, at
+      optio. Ipsa magnam earum, ex eveniet nemo libero ullam iure alias possimus
+      sed illum autem quo nisi omnis, accusamus veniam.
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Saludo",
+};
+</script>
+
+<style scoped></style>
