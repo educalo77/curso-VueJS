47a5b25342371ad23ad1c1df83e4f2c25a1582d7
educalo77 <educalo@outlook.com>
2021-02-13 13:35:54 -0300

    rutas dinamicas

---
+++ vuex/src/components/Posts.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Estas en posts</h1>
+    <p>{{ id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Posts",
+  data() {
+    const { id } = this.$route.params;
+    return {
+      id,
+    };
+  },
+};
+</script>
+
+<style></style>
--- vuex/src/routes.js
+++ vuex/src/routes.js
@@ -1,7 +1,9 @@
 import Inicio from "./components/Inicio";
 import Blog from "./components/Blog";
+import Posts from "./components/Posts";
 
 export const routes = [
   { path: "/inicio", component: Inicio },
   { path: "/blog", component: Blog },
+  { path: "/post/:id", component: Posts },
 ];
